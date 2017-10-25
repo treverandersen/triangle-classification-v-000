734a1f20415e16b492597756f374e7ef73f7929f
Trever Andersen <trever.r.andersen@gmail.com>
2017-10-25 20:08:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     if @length1 == 0 && @length2 == 0 && @length3 == 0
       raise TriangleError
     elsif @length1 <= 0 || @length2 <= 0 || @length3 <= 0
-      raise TriangleError.new("You did something wrong!")
+      raise TriangleError
     elsif @length1 + @length2 < @length3
       raise TriangleError
     elsif @length1 + @length3 <= @length2
@@ -36,9 +36,9 @@ end
 
 class TriangleError < StandardError
  # triangle error code
- def initialize(message)
-  # Call the parent's constructor to set the message
-  super(message)
- end
+ # def initialize(message)
+ #  # Call the parent's constructor to set the message
+ #  super(message)
+ # end
 
 end
